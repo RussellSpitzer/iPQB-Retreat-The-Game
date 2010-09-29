9f0907e1afff7051cc7de68985ade1188692442a
geoff <grollins@gmail.com>
2010-09-28 20:31:32 -0700

    Enemies fight back now.

--- Game.py
+++ Game.py
@@ -5,11 +5,17 @@ class ene (object):
 		self.sassy=sassy
 	def __str__(self):
 		return self.name
+	def attack(self):
+	    damage = 1
+	    print "%s attacks! %d points of damage" % (self.name, damage)
+	    return damage
 
 print "Generating the world of MAGIC"
 enemies= [ene("Tryptophan",1,"Not so"),ene("Nukey, The pore Complex",9001,"Extremely"),ene("Troll",10,"Very"),ene("Slime",1,"Don't Go There"),ene("Agonist Gradient",3,"Gradiently"),ene("LiCad",21,"Light Activatedly")]
 print ("Super-Reticulating-Splines")
 raw_input("Oh Yeah Done!.")
+
+user_hp = 5
 
 while (len(enemies)!=0):
 	print "The following enemies are around"
@@ -31,6 +37,12 @@ while (len(enemies)!=0):
 			enemies.remove(enemies[b])
 		else:
 			print str(enemies[b]) + " now has " + str(enemies[b].hitpoint) + " science points left."
+			user_hp -= enemies[b].attack()
+			if user_hp > 0:
+			    print "You have %d science points left." % (user_hp)
+			else:
+			    print "You are dead now. That's not very sassy at all."
+			    quit()
 	resp=raw_input("Is this awesome?")
 	print resp+"? Whateva..."
 	raw_input("Hit any key to continue")
