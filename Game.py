99f44801ec8e41f495923b8eaf39f14ee7dfce83
Geoff Rollins <grollins@gmail.com>
2020-03-10 13:34:03 -0500

    Here comes a new challenger

--- Game.py
+++ Game.py
@@ -1,17 +1,25 @@
 class ene (object):
-	def __init__(self,name,hitpoints,sassy):
+	def __init__(self,name,hitpoints,sassy,strength=1):
 		self.name=name
 		self.hitpoint=hitpoints
 		self.sassy=sassy
+		self.strength=strength
 	def __str__(self):
 		return self.name
 	def attack(self):
-	    damage = 1
+	    damage = self.strength
 	    print "%s attacks! %d points of damage" % (self.name, damage)
 	    return damage
 
 print "Generating the world of MAGIC"
-enemies= [ene("Tryptophan",1,"Not so"),ene("Nukey, The pore Complex",9001,"Extremely"),ene("Troll",10,"Very"),ene("Slime",1,"Don't Go There"),ene("Agonist Gradient",3,"Gradiently"),ene("LiCad",21,"Light Activatedly")]
+enemies= [ene("Tryptophan",1,"Not so"),
+		  ene("Nukey, The pore Complex",9001,"Extremely"),
+		  ene("Troll",10,"Very"),
+		  ene("Slime",1,"Don't Go There"),
+		  ene("Agonist Gradient",3,"Gradiently"),
+		  ene("LiCad",21,"Light Activatedly"),
+		  ene("Corona Virus",1e6,"Pandemically",100),
+		  ]
 print ("Super-Reticulating-Splines")
 raw_input("Oh Yeah Done!.")
 
@@ -19,16 +27,16 @@ user_hp = 5
 
 while (len(enemies)!=0):
 	print "The following enemies are around"
-	i=0;
+	i=0
 	for x in enemies:
 		print str(i)+":"+str(x) +" He is " + x.sassy +" sassy."
 		i+=1
 	try:
-		b=int(raw_input("Who will you kick ?: "))
+		b=int(raw_input("Who will you kick ? "))
 	except ValueError:
 		print "That ain't a number boyeeee. If you don't play by the rules you can't play at all."
 		quit()
-	if b not in  xrange(0,len(enemies)):
+	if b not in xrange(0,len(enemies)):
 		print "MISS"
 	else:
 		enemies[b].hitpoint-=1
@@ -43,8 +51,8 @@ while (len(enemies)!=0):
 			else:
 			    print "You are dead now. That's not very sassy at all."
 			    quit()
-	resp=raw_input("Is this awesome?")
+	resp=raw_input("Is this awesome? ")
 	print resp+"? Whateva..."
 	raw_input("Hit any key to continue")
 print "You win the ipqb retreat"
-		
+
